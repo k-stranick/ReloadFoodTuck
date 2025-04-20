85bb3ba6beb6253682f0314e4222c18d9c40951a
Kyle <kstranic@dtcc.edu>
2025-04-20 12:27:19 -0400

    trying to build [project for local testing

--- App.tsx
+++ App.tsx
@@ -4,15 +4,12 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import DrawerNavigator from "./src/navigation/DrawerNavigator";
 import { Provider } from "react-redux";
 import { store } from "./src/redux/store";
-import { ThemedView } from "./src/components/ThemedView";
+
 export default function App() {
   return (
-    // <View style={styles.container}>
-    //   <Text>Open up App.tsx to start working on your app!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
     <Provider store={store}>
       <SafeAreaProvider>
+        <StatusBar style="light" />
         <NavigationContainer>
           <DrawerNavigator />
         </NavigationContainer>
